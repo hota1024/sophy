f858faa23958f2d41e3e01b7f0031620bf543cfc
hota1024 <hotalog@hota1024.com>
2020-04-28 17:15:47 +0900

    🚚 Rename FileMeta.ts to NodeMeta.ts

---
+++ src/interfaces/Adapter/NodeMeta.ts
@@ -0,0 +1,16 @@
+import { NodeType } from '../../types/NodeType'
+
+/*
+ * NodeMeta type.
+ */
+export type NodeMeta = {
+  /**
+   * Node path.
+   */
+  path: string
+
+  /**
+   * Node type.
+   */
+  type: NodeType
+}
